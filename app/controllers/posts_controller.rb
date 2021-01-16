898f9a5af5ece973375771fe6581c0c883b75229
Mike Esposito <mikeesposito6852@gmail.com>
2021-01-16 11:51:51 -0800

    Add create action to Posts Controller and model to posts/new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:post)
+    end
 end
