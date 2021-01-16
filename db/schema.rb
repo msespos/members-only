07fd43478850c23863dff6eb374488bbe68e6fdf
Mike Esposito <mikeesposito6852@gmail.com>
2021-01-15 17:42:11 -0800

    Add foreign key to Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_012742) do
+ActiveRecord::Schema.define(version: 2021_01_16_014030) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
@@ -32,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_01_16_012742) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
