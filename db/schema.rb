2f4a1752ccc296477223fa65e2379364bb947d58
Mike Esposito <mikeesposito6852@gmail.com>
2021-01-31 06:47:04 -0800

    Convert database from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_16_200113) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
